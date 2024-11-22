35e91fefcdeb2d7d87dc9043aff9a6cd247c2162
DevMainPilot <newbotsforall@gmail.com>
2024-11-22 11:12:15 +0100

    Init project config

---
+++ src/db/queries.ts
@@ -0,0 +1,68 @@
+"server-only";
+
+export async function getUser( ){
+  try {
+    return "";
+  } catch (error) {
+
+  }
+}
+
+export async function createUser( ){
+
+  try {
+    return "";
+  } catch (error) {
+  }
+
+
+}
+
+export async function saveChat( ){
+  try {
+    return "";
+  } catch (error) {
+  }
+}
+
+export async function deleteChatById( ) {
+  try {
+      return "";
+  } catch (error) {
+
+  }
+}
+
+export async function getChatsByUserId( ){
+  try {
+      return "";
+  } catch (error) {
+
+  }
+}
+
+export async function getChatById( ){
+  try {
+      return "";
+  } catch (error) {
+
+  }
+}
+
+export async function createTables_temp( ){
+
+  try {
+      return "";
+  } catch (error) {
+  }
+}
+
+export async function createTables() {
+
+  try {
+      return "";
+  } catch (error) {
+    throw error;
+  }
+}
+
